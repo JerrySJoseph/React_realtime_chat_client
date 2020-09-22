1155ee74ba5b31fd8980551649958c53f55b8669
Jerry S Joseph <jerin.sebastian153@gmail.com>
2020-09-22 10:58:09 +0530

    Updates

---
+++ precache-manifest.342196f8d532c5680446612596d27ccf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24bded86273ca37c54dde9cae7c57ad1",
+    "url": "/React_realtime_chat_client/index.html"
+  },
+  {
+    "revision": "f2c1f667c017e515a15f",
+    "url": "/React_realtime_chat_client/static/css/main.b3f5e763.chunk.css"
+  },
+  {
+    "revision": "6d72fd6e2f0804770b9c",
+    "url": "/React_realtime_chat_client/static/js/2.a86f86da.chunk.js"
+  },
+  {
+    "revision": "a266fdc10c0e6f7474105f39d2223c69",
+    "url": "/React_realtime_chat_client/static/js/2.a86f86da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2c1f667c017e515a15f",
+    "url": "/React_realtime_chat_client/static/js/main.569e7501.chunk.js"
+  },
+  {
+    "revision": "72e8bb1651db519b0c9d",
+    "url": "/React_realtime_chat_client/static/js/runtime-main.bf9aa72f.js"
+  }
+]);
